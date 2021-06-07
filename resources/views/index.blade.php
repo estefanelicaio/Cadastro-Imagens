8dd3b38f2b824a272a5d01af650f0d3f8639707b
Caio Estefaneli <caioestefaneli@gmail.com>
2021-06-06 22:43:39 -0300

    Adicionando arquivos bootstrap e navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <title>Cadastro de Imagens</title>
+</head>
+<body>
+    
+<nav class="navbar fixed-top navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Fixed top</a>
+  </div>
+</nav>
+
+</body>
+</html>
