3aa7f29078a55cafeaf9f5b28ed3387ec5848426
Caio Estefaneli <caioestefaneli@gmail.com>
2021-06-25 18:49:22 -0300

    Alterada a página principal, separação do header e footer em arquivos diferentes e criação de um arquivo css personalizado,

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="text-muted py-5">
+    <div class="container">
+      <p class="float-end mb-1">
+        <a href="#">Voltar para o topo</a>
+      </p>
+      <p class="mb-1">&copy; Célula de Tecnologia da Informação</p>
+    </div>
+  </footer>
+
+
+
+</body>
+
+</html>
